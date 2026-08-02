0601e44194aa33fa411db23d1a4d26eda970655c
AnasTaha52 <anastaha699@gmail.com>
2026-08-02 11:41:07 +0500

    fix: load dotenv before app initialization and start server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
 import dotenv from "dotenv"
-import app  from "./app.js"
 
 dotenv.config({
     path: "./.env"
 })
 
+import app  from "./app.js"
+
 const port = process.env.PORT || 3000
 
 app.listen(port || 5000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+  console.log(`Server is running on port ${port}`);
 });
