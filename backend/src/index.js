7ad6d209e88f070967883b2dfc7c90f21f5d259e
AnasTaha52 <anastaha699@gmail.com>
2026-08-04 22:03:40 +0500

    initialize server entry point with database connection and environment configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,20 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "./.env"
-})
-
-import app  from "./app.js"
-
-const port = process.env.PORT || 3000
-
-app.listen(port || 5000, () => {
-  console.log(`Server is running on port ${port}`);
+  path: "./.env",
 });
+
+import app from "./app.js";
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection error", err);
+  });
