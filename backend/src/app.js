87569d1fb5a7d7fa65049becbb9de71e99d2acad
AnasTaha52 <anastaha699@gmail.com>
2026-08-05 12:51:53 +0500

    setup cookie parser middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,35 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 // Configurations
 
 //  app.use -> middleware
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true , limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser())
 
 // cors configurations
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN
-        ? process.env.CORS_ORIGIN.split(",").map((origin) => origin.trim()).filter(Boolean)
-        : "http://localhost:5173",
+      ? process.env.CORS_ORIGIN.split(",")
+          .map((origin) => origin.trim())
+          .filter(Boolean)
+      : "http://localhost:5173",
     credentials: true,
-    methods: ["GET" , "POST" , "PUT" , "PATCH" , "DELETE" , "OPTIONS"],
-    allowedHeaders: ["Authorization" , "Content-Type"]
-}))
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to NotesApp");
+});
 
-app.get("/" , (req , res) => {
-    res.send("Welcome to NotesApp");
-})
-
-
-export default app
+export default app;
