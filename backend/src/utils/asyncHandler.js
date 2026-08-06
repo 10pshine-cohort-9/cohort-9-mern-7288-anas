56e276e622651057cdf1805889f583ed4d0f1157
AnasTaha52 <anastaha699@gmail.com>
2026-08-06 12:22:35 +0500

    remove redundant comments

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -4,24 +4,4 @@ const asyncHandler = (requestHandler) => {
     }
 };
 
-// Two methods used for asynHandler Promises based and try/catch based.
-// One can be used based on the understanding
-// used comments for better understanding of higher order funstions. They accept funstion as a parameter and return a function
-
-
-// const asyncHandler = () => {}
-// const asyncHandler = (funct) => {() => {}}
-// const asyncHandler = (funct) => async () => {}
-
-// const asyncHandler = (func) => async (req , res , next) => {
-//     try {
-//         await func(req , res , next)
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             sucess: false,
-//             message: error.message
-//         })
-//     }
-// }
-
 export { asyncHandler };
