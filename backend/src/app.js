4bbbf3920e73ea87f6663d2f7dd132972b07ae25
AnasTaha52 <anastaha699@gmail.com>
2026-08-08 23:05:16 +0500

    setup user routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 
 // cors configurations
 
@@ -28,8 +28,10 @@ app.use(
   }),
 );
 
-app.get("/", (req, res) => {
-  res.send("Welcome to NotesApp");
-});
+// routes
+
+import userRoute from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoute);
 
 export default app;
