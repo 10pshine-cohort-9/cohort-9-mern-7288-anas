4a0e53a4d62abef66a14c21bf9fb96482c671f90
AnasTaha52 <anastaha699@gmail.com>
2026-08-11 11:53:50 +0500

    create basic pages like home , login , register and react-router configuration

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
+      <h1 className="text-3xl font-bold text-gray-800 dark:text-white">Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+
+const Register = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
+      <h1 className="text-3xl font-bold text-gray-800 dark:text-white">Register Page</h1>
+    </div>
+  );
+};
+
+export default Register;
