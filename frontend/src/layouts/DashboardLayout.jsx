e1decf9581ae2890e05886b9ef52699af80ecd5a
AnasTaha52 <anastaha699@gmail.com>
2026-08-29 12:20:24 +0500

    fix some issues

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -183,6 +183,7 @@ const DashboardLayout = () => {
           tabIndex={0}
           className="fixed inset-0 z-20 bg-slate-900/30 backdrop-blur-xs md:hidden"
           onClick={() => setIsSidebarOpen(false)}
+          role="button"
           onKeyDown={(e) => {
             if (e.key === "Enter" || e.key === " ") {
               e.preventDefault();
